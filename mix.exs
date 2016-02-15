f034e32593f823bb5ad45a879c1f9b14e5cb1db4
Sergey Gernyak <sergeg1990@gmail.com>
2016-02-15 21:47:52 +0200

    Setted up some options for Hex package

--- mix.exs
+++ mix.exs
@@ -7,26 +7,30 @@ defmodule ExFabricators.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     description: description,
+     package: package
+   ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     []
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    Easy way to cook your structs for tests
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Sergio Gernyak"],
+      links: %{"GitHub" => "https://github.com/alterego-labs/ex_fabricators"}
+    ]
   end
 end
